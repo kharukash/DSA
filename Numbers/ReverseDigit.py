ca7674d99e7f9f160377b7cc076e12fdd2461989
Ashish <ashishkharuk2001@gmail.com>
2025-12-20 23:27:55 +0530

    Solution

--- Numbers/ReverseDigit.py
+++ Numbers/ReverseDigit.py
@@ -1,6 +1,26 @@
+#If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+# Without list and String   
+Time : O(d)  # loop runs d(digits) times
+Space: O(1) 
+
+class Solution:
+    def reverseNumber(self, n):
+        rev = 0
+        neg = n < 0   # True if say -123 so that later if neg it gives -321
+        n = abs(n)    # if -123 gives 123
+        while n > 0:
+            rev = rev * 10 + (n % 10)      # % gives remainder
+            n //= 10     # // gives quotient
+        
+        if rev > (2**31 - 1):
+            return 0
+        return -rev if neg else rev
+
+
 # Usine List reverse()  
-Time : O(d)
-Space: O(d)     
+#Time : O(d)
+#Space: O(d) 
+    
 class Solution:
     def reverse(self, n: int) -> int:
        neg = n<0
@@ -8,26 +28,20 @@ class Solution:
        y = list(str(n))
        y.reverse()
        rev = int("".join(y))
-       if rev > (2**31 - 1):    #rev should give 0 if > 32-bit signed integer range
+       if rev > (2**31 - 1):    
            return 0
-       return -rev if neg else rev
- 
+       return -rev if neg else rev  
 
-# Without list and String   
-Time : O(d)  # loop runs d(digits) times
-Space: O(1)  
+
+
+#Using String:
+#Space and Time : O(d)
+
 class Solution:
-    def reverseNumber(self, n):
-        rev = 0
-        neg = n < 0   # True if say -123 so that later if neg it gives -321
-        n = abs(n)    # if -123 gives 123
-        while n > 0:
-            rev = rev * 10 + (n % 10)
-            n //= 10
-        
-        if rev > (2**31 - 1):
-            return 0
-        return -rev if neg else rev
-    
-
-            
+    def reverse(self, n: int) -> int:
+        neg = n<0
+        n = abs(n)
+        rev = str(n)[::-1]
+        rev = int(rev)
+        if rev > (2**31)-1:
+            return 0   
